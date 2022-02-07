cca3657fbe29b4d3c018da851ff91bd37b0f960b
Gustavo <gustagui.dev@gmail.com>
2022-02-07 11:23:57 -0300

    clean

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,39 +1,9 @@
 const indexController = {
-    exibirHome: (req, res, next) => {
-        res.render('home', { title: 'Portfolio - Gustavo Silva' });
-    },
-    exibirProjects: (req, res) => {
-        res.render('projects', { title: 'Portifolio - Gustavo Silva'});
-    },
     exibirIndex: (req, res) => {
         res.render('index')
     },
-    exibirMercearia: (req, res) => {
-        res.render('mercearia')
-    },
-    exibirBolachas: (req, res) => {
-        res.render('bolachas')
-    },
     exibirSorteio: (req, res) => {
         res.render('sorteio')
-    },
-    exibirBalas: (req, res) => {
-        res.render('balas')
-    },
-    exibirBebidas: (req, res) => {
-        res.render('bebidas')
-    },
-    exibirDoces: (req, res) => {
-        res.render('doces')
-    },
-    exibirEmbalagem: (req, res) => {
-        res.render('embalagem')
-    },
-    exibirSaudaveis: (req, res) => {
-        res.render('saudaveis')
-    },
-    exibirSalgadinho: (req, res) => {
-        res.render('salgadinho')
     },
 };
 
--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', indexController.exibirIndex);
-router.get('/home', indexController.exibirHome);
-router.get('/projects', indexController.exibirProjects);
 router.get('/index', indexController.exibirIndex);
-router.get('/mercearia', indexController.exibirMercearia);
-router.get('/bolachas', indexController.exibirBolachas);
 router.get('/sorteio', indexController.exibirSorteio);
-router.get('/balas', indexController.exibirBalas);
-router.get('/bebidas', indexController.exibirBebidas);
-router.get('/doces', indexController.exibirDoces);
-router.get('/salgadinho', indexController.exibirSalgadinho);
-router.get('/embalagem', indexController.exibirEmbalagem);
-router.get('/saudaveis', indexController.exibirSaudaveis);
 
 module.exports = router;
